22371c568fb31b2e30c537052c688c25277d3696
LeeVaughn <dlvprogramming@gmail.com>
2022-09-14 15:35:47 -0700

    Update characters replaced

--- rename_files.py
+++ rename_files.py
@@ -4,4 +4,4 @@ path = "./files_to_rename"
 
 # rename each file by replacing spaces with underscores
 for filename in os.listdir(path):
-    os.rename(os.path.join(path,filename),os.path.join(path, filename.replace(' ', '_')))
+    os.rename(os.path.join(path,filename),os.path.join(path, filename.replace(' ', '_').replace(',', '').replace('&', 'and').replace("'", "").replace('!', '').replace('+', '_').replace('(', '_').replace('(', '_').replace(')', '_').replace('___', '_').replace('__', '_')))
