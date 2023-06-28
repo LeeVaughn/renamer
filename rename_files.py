a9fc1f315f00dbff2decaa791116a4df1f89d07d
LeeVaughn <dlvprogramming@gmail.com>
2023-06-28 09:59:29 -0700

    Revert change to add regex

--- rename_files.py
+++ rename_files.py
@@ -5,5 +5,4 @@ path = "./files_to_rename"
 
 # rename each file by replacing anything that isn't a letter or number with an underscore
 for filename in os.listdir(path):
-    new_filename = re.sub('[^A-Za-z0-9]+', '_', filename)
-    os.rename(os.path.join(path,filename),os.path.join(path, new_filename))
+    os.rename(os.path.join(path,filename),os.path.join(path, filename.replace(' ', '_').replace(',', '').replace('：', '').replace('&', 'and').replace("'", "").replace('!', '').replace('+', '_').replace('(', '_').replace(')', '_').replace('[', '_').replace(']', '_').replace('|', '_').replace('⧸', '_').replace('｜', '_').replace('__', '_').replace('___', '_').replace('____', '_')))
