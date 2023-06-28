1f9e290d5b024a085b29853f382c443b7cfb4854
LeeVaughn <dlvprogramming@gmail.com>
2023-06-28 09:43:19 -0700

    Add regex to remove non-letters or numbers

--- rename_files.py
+++ rename_files.py
@@ -1,7 +1,9 @@
 import os
+import re
 
 path = "./files_to_rename"
 
-# rename each file by replacing spaces with underscores
+# rename each file by replacing anything that isn't a letter or number with an underscore
 for filename in os.listdir(path):
-    os.rename(os.path.join(path,filename),os.path.join(path, filename.replace(' ', '_').replace(',', '').replace('&', 'and').replace("'", "").replace('!', '').replace('+', '_').replace('(', '_').replace('(', '_').replace(')', '_').replace('___', '_').replace('__', '_')))
+    new_filename = re.sub('[^A-Za-z0-9]+', '_', filename)
+    os.rename(os.path.join(path,filename),os.path.join(path, new_filename))
